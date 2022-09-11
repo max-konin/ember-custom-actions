c29b29fd6426aeec882984817146009b51673d17
Max Konin <maxim21214@gmail.com>
2022-09-11 22:20:40 +0700

    Remove serializers

--- addon/index.js
+++ addon/index.js
@@ -1,8 +1,6 @@
 import resourceAction from './actions/resource';
 import modelAction from './actions/model';
 import customAction from './actions/custom';
-import JSONAPISerializer from 'ember-custom-actions/serializers/json-api';
-import RESTSerializer from 'ember-custom-actions/serializers/rest';
 import AdapterMixin from 'ember-custom-actions/mixins/adapter';
 
 export {
